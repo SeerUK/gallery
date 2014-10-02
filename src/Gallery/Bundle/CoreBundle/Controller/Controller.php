95fbf72602acc8c64ed87fa43de376ead2e933d3
Elliot Wright <wright.elliot@gmail.com>
2014-10-02 21:57:19 +0100

    CoreBundle and Vagrant setup added and configured.

---
+++ src/Gallery/Bundle/CoreBundle/Controller/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the gallery package.
+ *
+ * (c) Elliot Wright <elliot@elliotwright.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gallery\Bundle\CoreBundle\Controller;
+
+/**
+ * Controller
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+abstract class Controller
+{
+}
---
+++ src/Gallery/Bundle/CoreBundle/GalleryCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gallery\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GalleryCoreBundle extends Bundle
+{
+}
